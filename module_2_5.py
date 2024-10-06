4829e795e2bc71b801f504d1640894bcb41bdfe5
cherdak444 <67187881+cherdak444@users.noreply.github.com>
2024-10-06 15:44:38 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,16 @@
+def get_matrix(n, m, value,):
+    matrix = []
+    for i in range(0, n):
+        list_ = []
+        matrix.append(list_)
+        for j in range(0, m):
+            list_.append(value)
+    print(matrix)
+
+get_matrix(2,4,34)
+get_matrix(5,6,23)
+get_matrix(10,4,66)
+
+
+
+
