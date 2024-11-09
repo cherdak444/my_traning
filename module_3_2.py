36bfa1e0dc0137fe9d5ff026cc5197ab7cccbc0e
cherdak444 <67187881+cherdak444@users.noreply.github.com>
2024-11-09 20:22:03 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,13 +1,17 @@
-def send_email(message, recipient, *, sender = "university.help@gmail.com"):
-    if not recipient.endswith((".com", ".ru", ".net")) and "@" not in recipient:
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if "@" not in recipient or "." not in recipient:
+        print(f"Невозможно отправить письмо с адреса {sender} на adres {recipient}")
     elif sender == recipient:
-        print("Не возможно отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    else:
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        print("Нельзя отправить письмо самому себе!")
+    elif "@mail.ru" and "gmail.com" not in sender:
+        print(f"Невозможно отправить письмо с адреса {sender} на adres {recipient}")
+        return
 
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на adres {recipient}")
+    
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
