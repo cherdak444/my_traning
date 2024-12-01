b06e8b0625e69d78901f42dec46ca6fb38630f70
cherdak444 <67187881+cherdak444@users.noreply.github.com>
2024-12-01 21:49:09 +0300

    Работа над ошибками module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,30 +1,19 @@
 calls = 0
-
 def count_calls():
     global calls
     calls += 1
-
 def string_info(string):
-    length = len(string)
-    uppercase_string = string.upper()
-    lowercase_string = string.lower()
-    return (length, uppercase_string, lowercase_string)
+    count_calls() 
+    return (len(string), string.upper(), string.lower())
 
 def is_contains(string, list_to_search):
-    global calls
-    if string.lower() in [s.lower() for s in list_to_search]:
-        count_calls()
-        return True
-    else:
-        count_calls()
-        return False
+    count_calls()  
+    string_lower = string.lower()  
+    list_to_search_lower = [item.lower() for item in list_to_search]  
+    return string_lower in list_to_search_lower
 
 print(string_info('Capybara'))
-
 print(string_info('Armageddon'))
-
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
-
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 print(calls)
